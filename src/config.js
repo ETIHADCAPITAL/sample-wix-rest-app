466fcc5abfc23b0dd0c12779e0e4c1bf44a3f662
ETIHADCAPITAL <62247374+ETIHADCAPITAL@users.noreply.github.com>
2020-04-06 15:27:51 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,14 @@
 // TODO: Update APP_ID with your application ID (can be found in in Wix Developers under Workspace/OAuth)
-const APP_ID = 'Enter your APP ID here';
+const APP_ID = 'e9ae58d3-bdb4-4935-b7b6-ec7de9f6ff1b';
 
 // TODO: Update PUBLIC_KEY with your public Key (can be found under Workspace/Webhooks/Public-Key)
-const PUBLIC_KEY = 'Enter your PUBLIC KEY here';
+const PUBLIC_KEY = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlpz+YfP1xrZUely058rb
+WAqxX2zC8tdZ7hqLDGusrjsNi1+fcRyWH2/HnfnTlBV39n03SxjDbS6xPHmLSpv4
+Kg4ZOtg+++MjOb8XB9uZHP2D1sm9Z3pfD6TbBd2EaAyLvNLBZHXm9UmHoNPweLE1
+6bTwrnmRxw1vx2vCHHUPL5xat+Eg3r0Uw2ykwVo8wS9psyFSxTWY9HqhmFbsfphf
++2sF3LtKC65QgX5D+Dq6LLwM+feZBdwlWGzePyxr0MVWdOQB7sBU0qFpDa8Dkf+H
+/+Qw/XoJ+EmVNm0kdFMYb+Lt5xmOmTY/9G3Exi6i9CnEHPLOmehJW/KCM2oNbGnN
+LQIDAQAB';
 
 exports.APP_ID = APP_ID;
 exports.PUBLIC_KEY = PUBLIC_KEY;
